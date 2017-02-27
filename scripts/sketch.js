f03db2080b729c89cb66a4f818ce226cfbe5fcc6
qwerji <bentswanson@gmail.com>
2017-02-26 22:02:09 -0800

    http

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -26,7 +26,7 @@ const words = [
     'CraigReynolds','Braitenberg','steer=desire-vel',
     'vehicles','schfiftyfive','odie','poinciana'
 ]
-const apiURL = `https://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`
+const apiURL = `http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5`
 
 function preload() {
     font = loadFont("fonts/AvenirNextLTPro-Demi.otf") 
