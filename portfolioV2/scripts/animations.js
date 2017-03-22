a4e660bc3fd999e2ed8524d0437dfe01bb012e74
qwerji <bentswanson@gmail.com>
2017-03-21 21:54:46 -0700

    portfolio project page complete

--- portfolioV2/scripts/animations.js
+++ portfolioV2/scripts/animations.js
@@ -7,35 +7,20 @@
         loadingScreen = document.getElementsByClassName('loading')[0],
         sections = document.getElementsByTagName('section'),
         main = document.getElementsByTagName('main')[0],
+        main2 = document.getElementsByTagName('main')[1],
         snail = document.getElementById('snail'),
+        soundcloud = document.getElementById('soundcloud'),
+        bandcamp = document.getElementById('bandcamp'),
+        homeLink = document.getElementById('homeLink'),
+        projectLink = document.getElementById('projectLink'),
+        projectButtons = document.getElementsByClassName('projectButtons'),
         sectionHash = {}, // Stores the pointers to each section (each square)
         sectionLists = { // Stores the orderings for each breakpoint
             small: ["logo", "whoami", "softwareDev", "projects", "musician", "space8", "contact"],
             medium: ["logo", "whoami", "softwareDev", "projects", "contact", "musician"],
             all: ["logo", "photo", "space3", "whoami", "musician", "projects", "softwareDev", "space8", "space9", "space10", "contact", "space12"],
-            isACard: {"logo":true,"photo":true,"whoami":true,"musician":true,"softwareDev":true}
+            isACard: {"logo":true,"photo":true,"whoami":true,"musician":true,"softwareDev":true,"food":true,"done-for":true,"snail-fantasy":true,"blackbelts":true}
         }
-
-    // Adds the section elements to the hash
-    for (let i = 0; i < sections.length; i++) {
-        sectionHash[sections[i].id] = sections[i]
-    }
-
-    // Set up card flip event listener
-    for (let _section in sectionHash) {
-        // Apply the listener/cursor change only if it is a card
-        if (sectionLists.isACard[_section]) {
-            const section = sectionHash[_section]
-            applyCardFlipListener(section)
-            section.style.cursor = "pointer"
-        }
-    }
-    function applyCardFlipListener(section) {
-        section.addEventListener("click", event => {
-            const classList = section.children[0].classList
-            classList.contains("flipped") ? classList.remove("flipped") : classList.add("flipped")
-        })
-    }
 
     // Hides the ugly setup for the card flips
     setTimeout(() => {
@@ -45,6 +30,26 @@
             loadingScreen.style.display = 'none'
         }, 600)
     }, 500)
+
+    // Adds the section elements to the hash
+    for (let i = 0; i < sections.length; i++) {
+        sectionHash[sections[i].id] = sections[i]
+    }
+
+    // Set up card flip event listener
+    for (let _section in sectionHash) {
+        // Apply the listener only if it is a card
+        if (sectionLists.isACard[_section]) {
+            const section = sectionHash[_section]
+            applyCardFlipListener(section)
+        }
+    }
+    function applyCardFlipListener(section) {
+        section.addEventListener("click", event => {
+            const classList = section.children[0].classList
+            classList.contains("flipped") ? classList.remove("flipped") : classList.add("flipped")
+        })
+    }
 
     // Handles the animations for the navigation opening and closing
     let navOpen = false
@@ -58,6 +63,7 @@
             hamburger.style.width = '21px'
             hamburger.style.height = '21px'
             navButton.classList.remove('openNavButton')
+            document.body.classList.remove('noScroll')
         } else {
             navOpen = true
             nav.classList.add('open')
@@ -68,6 +74,7 @@
             hamburger.style.height = '15px'
             navButton.classList.add('openNavButton')
             navButton.classList.remove('navButtonHover')
+            document.body.classList.add('noScroll')
         }
     }
 
@@ -83,6 +90,20 @@
         brackets[0].classList.remove('bracketHoverLeft')
         brackets[1].classList.remove('bracketHoverRight')
         navButton.classList.remove('navButtonHover')
+    }
+
+    soundcloud.onmouseover = () => {
+        soundcloud.src = 'images/soundcloud-hover.svg'
+    }
+    soundcloud.onmouseout = () => {
+        soundcloud.src = 'images/soundcloud.svg'
+    }
+
+    bandcamp.onmouseover = () => {
+        bandcamp.src = 'images/bandcamp-hover.svg'
+    }
+    bandcamp.onmouseout = () => {
+        bandcamp.src = 'images/bandcamp.svg'
     }
 
     // Helper function to remove all child nodes from a DOM element
@@ -134,5 +155,22 @@
     }
     // Set the initial state
     window.onresize()
+
+    sectionHash['projects'].onclick = () => {
+        main.classList.add('noScroll')
+        main2.classList.add('flyIn')
+    }
+
+    homeLink.onclick = () => {
+        main2.classList.remove('flyIn')
+        main.classList.remove('noScroll')
+        navOpen = true
+        navButton.onclick()
+    }
+    projectLink.onclick = () => {
+        sectionHash['projects'].onclick()
+        navOpen = true
+        navButton.onclick()
+    }
 
 })()
